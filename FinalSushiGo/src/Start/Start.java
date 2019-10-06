b7f2702ed3e3d57e48f97106ae5b0c5988dadf55
mightymosdan <danielangelestiu@gmail.com>
2019-10-06 13:48:32 -0600

    Recoded Start.java so that if you input anything other than 1 or 2 as the input for your choices when asked if you want to play player vs player or player vs ai, then the program ends

--- FinalSushiGo/src/Start/Start.java
+++ FinalSushiGo/src/Start/Start.java
@@ -14,39 +14,39 @@ public class Start {
 	private static Scanner user_input;
 	private static GameConfiguration game;
 
-	public static void main(String[] args){
-		
-		user_input = new Scanner(System.in);
-		int selection, players;
-		
-		System.out.println("Sushi GO!");
-		System.out.println("1) Player vs Player");
-		System.out.println("2) Player vs AI");
+	public static void main(String[] args) {
 
-		
-        do {
-            System.out.print("Please Choose One of the Options: ");
-            while (!user_input.hasNextInt()) {
-                String input = user_input.next();
-                System.out.printf("\"%s\" is not a valid number.\n", input);
-            }
-            selection = user_input.nextInt();
+        user_input = new Scanner(System.in);
+        int selection, players;
 
-            if(selection == 1 || selection == 2) {
-                do {
-                    System.out.print("Enter the number of players between 2-4: ");
-                    while (!user_input.hasNextInt()) {
-                        String input = user_input.next();
-                        System.out.printf("\"%s\" is not a valid number.\n", input);
-                    }
-                    players = user_input.nextInt();
+        System.out.println("Sushi GO!");
+        System.out.println("1) Player vs Player");
+        System.out.println("2) Player vs AI");
+        System.out.print("Please Choose One of the Options: ");
+        while (!user_input.hasNextInt()) {
+            String input = user_input.next();
+            System.out.printf("\"%s\" is not a valid number.\n", input);
+        }
+
+        selection = user_input.nextInt();
+
+        if (selection == 1 || selection == 2) {
+            do {
+                System.out.print("Enter the number of players between 2-4: ");
+                while (!user_input.hasNextInt()) {
+                    String input = user_input.next();
+                    System.out.printf("\"%s\" is not a valid number.\n", input);
                 }
-                while (players < 2 || players > 4);
-
-
-                game = new GameConfiguration(selection, players);
-                game.play();
+                players = user_input.nextInt();
             }
-            else{}
-        ]
+            while (players < 2 || players > 4);
+
+
+            game = new GameConfiguration(selection, players);
+            game.play();
+        } else {
+            System.out.println("You did not choose 1) Player vs Player or 2) Player vs AI. Please reload the program and try again");
+        }
+    }
+
 }
