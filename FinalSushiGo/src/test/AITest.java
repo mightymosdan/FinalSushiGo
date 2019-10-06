63eefffcd16394a2c4dbbb7a2a42f8451655c5ce
mightymosdan <danielangelestiu@gmail.com>
2019-10-06 01:31:05 -0600

    Addes Test case for AI.java class. This runs 2 tests, the first to make sure that the AI object is in fact an AI object. The second to make sure that AI objects are not the same as Human objects.

---
+++ FinalSushiGo/src/test/AITest.java
@@ -0,0 +1,33 @@
+package test;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import TEXTpackage.*;
+
+import java.util.LinkedList;
+
+
+public class AITest {
+
+    @Test
+    public void testCreateAI(){
+        LinkedList<String>[] testHands = new LinkedList[0];
+        LinkedList<String>[] hands = new LinkedList[0];
+
+        AI _AI = new AI(0, hands);
+        AI testAI = new AI(0,testHands);
+
+        assertEquals("These are not the same objects, but they should be.",testAI.getClass(),_AI.getClass());
+
+    }
+
+    @Test
+    public void testAIisNotHuman(){
+        LinkedList<String>[] hands = new LinkedList[0];
+
+        AI _AI = new AI(0, hands);
+        Human testHuman = new Human(1);
+
+        assertNotSame("These should not be the same object.", testHuman, _AI);
+
+    }
+}
